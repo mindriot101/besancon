5a73131ed8ed94181a7185cafcdfe427f84687a8
Simon Walker <s.r.walker101@googlemail.com>
2014-02-26 23:26:23 +0000

    Improves testing split

--- testing/test_besancon.py
+++ testing/test_besancon.py
@@ -3,20 +3,28 @@ from besancon.spectraltype import SpectralType
 from unittest import TestCase
 import pytest
 
-def test_set_email():
-    b = Besancon(email="test@example.com")
-    assert b.email == "test@example.com"
+def setup_module(module):
+    module.BESANCON = Besancon(email='test@example.com')
 
-class TestSetters(TestCase):
+class BesanconTester(TestCase):
     def setUp(self):
-        self.b = Besancon(email='test@example.com')
+        self.b = BESANCON
 
+
+class TestEmail(BesanconTester):
     def test_query_without_email_fails(self):
+        b = Besancon()
         with pytest.raises(RuntimeError) as err:
-            self.b.query(lat=20.3, long=51.2, area=64.0)
+            b.query(lat=20.3, long=51.2, area=64.0)
 
         assert "no email set" in str(err).lower()
 
+
+    def test_set_email(self):
+        assert self.b.email == "test@example.com"
+
+
+class TestSpectralType(BesanconTester):
     def test_spectral_type_limit(self):
         self.b = Besancon()
         self.b.limit_spectral_type("1.0", "9.5")

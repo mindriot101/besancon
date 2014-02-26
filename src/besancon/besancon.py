a09b2302138ec3121fb8a2ada92d532cf750f961
Simon Walker <s.r.walker101@googlemail.com>
2014-02-26 23:22:58 +0000

    Sets limit defaults

--- src/besancon/besancon.py
+++ src/besancon/besancon.py
@@ -3,7 +3,8 @@ from .spectraltype import SpectralType
 class Besancon(object):
     def __init__(self, email=None):
         self.email = email
-        self.spectral_type_limits = [None, None]
+        self.spectral_type_limits = [SpectralType.LOWER_LIMIT,
+                SpectralType.UPPER_LIMIT]
     
     def limit_spectral_type(self, lower=None, upper=None):
         lower_spectral_type = SpectralType.from_string(lower) if lower else None

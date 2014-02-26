8b7b09ab157af93465136b999face066feb7dfdc
Simon Walker <s.r.walker101@googlemail.com>
2014-02-26 22:56:52 +0000

    renames module

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup
 from glob import glob
 
-setup(name='besancon_query',
+setup(name='besancon',
         version='0.0.1',
         author='Simon Walker',
         author_email='s.r.walker101@googlemail.com',
-        packages=['besancon_query', ],
+        packages=['besancon', ],
         package_dir={'': 'src'},
-        scripts=glob('bin/*.py'),
         long_description=open('README.markdown').read(),
         )
